2b9500124af9d603335dbf300006f20017d07650
Steve Tyree <styree@commercialtribe.com>
2016-10-04 15:33:09 -0600

    Material Test Font

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,32 +5,38 @@
 
         // Project configuration.
         grunt.initConfig({
-			clean: {
-				options:{
-					'no-write': false
-				},
-				clean: ['/build/font/']
-			},
-			
-			svgicons2svgfont: {
-				options: {
-					fontName: 'newIconFont',
-					normalize: true
-				},
-				build: {
-					src: 'src/icons/*.svg',
-					dest: 'build/font/'
-				},
-			},
+            clean: {
+                options: {},
+                clean: ['/build/font/']
+            },
+
+            svgicons2svgfont: {
+                options: {
+                    fontName: 'newIconFont',
+                    normalize: true
+                },
+                build: {
+                    src: 'src/icons/**/*.svg',
+                    dest: 'build/'
+                },
+            },
+
+            svg2ttf: {
+                svg2ttf: {
+                    src: 'build/newIconFont.svg',
+                    dest: 'build/'
+                }
+            }
 
         }); // end config
 
         // These plugins provide necessary tasks.
-		grunt.loadNpmTasks('grunt-svgicons2svgfont');
-		grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-svgicons2svgfont');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-svg2ttf');
 
         // Default task.
-        grunt.registerTask('default', ['clean', 'svgicons2svgfont']);
+        grunt.registerTask('default', ['clean', 'svgicons2svgfont', 'svg2ttf']);
 
     };
 
